99f89dfb0aa3fe70b49994610449db56d895364b
yasuko8017 <yasuko8017@hotmail.com>
2020-02-14 08:34:43 +0800

    引入vuetify 實作ToolBar

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-toolbar
+    absolute
+    color="teal darken-3"
+    dark
+    scroll-off-screen
+    scroll-target="#scrolling-techniques"
+  >
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>
+      Cherri
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn outline>
+      登出
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'ToolBar',
+}
+</script>
+
+<style scoped>
+
+</style>
